ab5ab28495e9fbab8959bbc164907612f56a408d
FotoDiagX <thiagofranssa@gmail.com>
2025-03-18 16:02:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -127,9 +127,9 @@ if uploaded_file is not None:
 
         # Ajustar o tamanho da fonte das porcentagens
         for text in ax.texts:
-            text.set_fontsize(5)  # Ajuste o valor para o tamanho desejado
+            text.set_fontsize(10)  # Ajuste o valor para o tamanho desejado
     
-        ax.set_title('Probabilidades de Diagnóstico', fontsize = 7)
+        ax.set_title('Probabilidades de Diagnóstico', fontsize = 15)
         st.pyplot(fig)
 
 else:
