3a5b693859d112f3a6c6836b51f0cb44878b618a
FotoDiagX <thiagofranssa@gmail.com>
2025-03-18 15:30:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if uploaded_file is not None:
             plt.gca().invert_xaxis()
             plt.title('Espectro FTIR', pad=10, fontsize=30, fontname='Cambria')
             plt.xlabel('Número de Onda ($\mathregular{cm^-¹}$)', labelpad=17, fontsize=26, fontname='Cambria')
-            plt.ylabel('Transmitância Normalizada', labelpad=15, fontsize=28, fontname='Cambria')
+            plt.ylabel('Transmitância (%)', labelpad=15, fontsize=28, fontname='Cambria')
             plt.xticks(np.arange(900, 1800 + 100, 100), fontsize=18, fontname='Cambria')
             plt.gca().tick_params(axis='x', pad=20)  # Ajusta o espaço entre os rótulos e a linha
             plt.xlim(1800, 900)
